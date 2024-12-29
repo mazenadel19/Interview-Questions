7c04e6de305392fefee9f4e8d834a42024627723
Mazen <mazenadel19@gmail.com>
2024-12-29 03:45:07 +0200

    magic cookie factory

--- 2024/2024-12-23/magic_cookie_factory.js
+++ 2024/2024-12-23/magic_cookie_factory.js
@@ -1,4 +1,13 @@
-export function countPerfectlyRoundCookies(params) {}
+// At the Magic Cookie Factory, cookies are baked in factorial quantities. A cookie is "perfectly round" if its size ends with a zero. Write a function to determine how many perfectly round cookies will be made when baking with n! ingredients.
+
+export function countPerfectlyRoundCookies(num) {
+  let count = 0;
+  while (num >= 5) {
+    count += Math.floor(num / 5);
+    num = Math.floor(num / 5);
+  }
+  return count;
+}
 
 countPerfectlyRoundCookies(5); // 1
 countPerfectlyRoundCookies(10); // 2
